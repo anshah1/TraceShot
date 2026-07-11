9ea5461000e9db7570c486c21793b7e88f56ed51
anshah1 <anshshah2025@gmail.com>
2026-07-11 14:42:08 -0400

    boilerplate set up (tasks 1 and 2)

---
+++ extension/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>TraceShot</h1>
+      <p>Screenshot tracking extension</p>
+      <button>Capture</button>
+    </div>
+  )
+}
+
+export default App
